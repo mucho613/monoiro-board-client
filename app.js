0f28ec3a9e380a519c0758d72dea650238edc35c
Daisuke Yamamura <mucho613@gmail.com>
2019-07-07 22:01:45 +0000

    ファイル移動

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const PORT = process.env.Port || 8080;
+
+http.listen(PORT, () => {
+  console.log('Running at Port ' + PORT);
+});
+
+// 静的ファイルのルーティング
+app.use(express.static(path.join(__dirname, 'public')));
+
+// その他のリクエストに対する404エラー
+app.use((req, res) => {
+  res.sendStatus(404);
+});
+
+io.on('connection', function (socket) {
+  // クライアントからメッセージ受信
+  socket.on('clear send', function () {
+    socket.broadcast.emit('clear user');
+  });
+
+  // クライアントからメッセージ受信
+  socket.on('server send', function (msg) {
+    // console.log(msg);
+    socket.broadcast.emit('send user', msg);
+  });
+
+  // 切断
+  socket.on('disconnect', function () {
+    // io.sockets.emit('user disconnected');
+  });
+});
--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,10 @@
 (function() {
+  window.onpageshow = function(event) {
+    if (event.persisted) {
+      window.location.reload()
+    }
+  };
+
   //HTML上の canvas タグを取得
   let canvas = document.getElementById('canvas');
 
@@ -6,38 +12,43 @@
   let eraserBtn = document.getElementById('eraser-btn');
   let allEraseBtn = document.getElementById('all-erase-btn');
 
+  let canvasWrapper = document.getElementById('canvas-wrapper');
+
   let debugInfo = document.getElementById('debug-info');
 
   let pen = document.getElementById('pen');
 
-  canvas.addEventListener('touchmove', (e) => {    
-    let azimuthAngle = e.touches[0].azimuthAngle;
-    let altitudeAngle = e.touches[0].altitudeAngle;
+  let socket = io.connect('http://198.13.35.247:8080');
 
-    pen.style.transform = `rotateZ(${azimuthAngle}) rotateY(${altitudeAngle})`;
+  canvas.addEventListener('touchmove', (e) => {    
+    let azimuthAngle = e.touches[0].azimuthAngle || 0;
+    let altitudeAngle = e.touches[0].altitudeAngle || 0;
   });
 
   let defaultColor = "#555555";
   let defaultAlpha = 1.0;
-  let thicknessCoefficient = 1;
+  let thicknessCoefficient = 4;
 
-  let canvasWidth = 800;
-  let canvasHeight = 500;
+  let canvasWidth = 2000;
+  let canvasHeight = 2000;
+
+  let stopScroll = function(e) {
+    e.preventDefault();
+  }
 
   penBtn.addEventListener('click', (e) => {
-    thicknessCoefficient = 1;
+    thicknessCoefficient = 4;
     defaultColor = "#555555";
   });
 
   eraserBtn.addEventListener('click', (e) => {
-    thicknessCoefficient = 4;
+    thicknessCoefficient = 32;
     defaultColor = "#f5f5f5";
   });
 
   allEraseBtn.addEventListener('click', (e) => {
-    ctx.beginPath();
-    ctx.fillStyle = "#f5f5f5";
-    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+    allClear();
+    socket.emit('clear send');
   });
 
 
@@ -61,6 +72,12 @@
   window.addEventListener('scroll', (e) => {
     scrolled = true;
   });
+
+  canvasWrapper.addEventListener('scroll', (e) => {
+    scrolled = true;
+  });
+
+  canvas.addEventListener('touchmove', stopScroll, { passive: false });
 
   canvas.addEventListener('mousedown', (e) => {
     if(!rectX || scrolled) firstDraw(e);
@@ -96,6 +113,7 @@
 
   canvas.addEventListener('touchstart', (e) => {
     if(e.touches[0].touchType == 'direct') {
+      canvas.removeEventListener('touchmove', stopScroll);
       return;
     }
     if(!rectX || scrolled) firstDraw(e);
@@ -112,9 +130,13 @@
       "<br>angle: " + e.touches[0].rotationAngle +
       "<br>azimuthAngle: " + e.touches[0].azimuthAngle +
       "<br>altitudeAngle: " + e.touches[0].altitudeAngle;
-    // console.log(e);
 
-    if(e.touches[0].touchType == 'direct') return;
+    if(e.touches[0].touchType == 'direct') {
+      canvas.removeEventListener('touchmove', stopScroll);
+      return;
+    } else if(e.touches[0].touchType == 'stylus') {
+      canvas.addEventListener('touchmove', stopScroll, { passive: false });
+    }
 
     let X = ~~(e.changedTouches[0].clientX - rectX);
     let Y = ~~(e.changedTouches[0].clientY - rectY);
@@ -127,7 +149,12 @@
   });
 
   canvas.addEventListener('touchend', (e) => {
-    if(e.touches[0].touchType == 'direct') return;
+    if(e.touches[0].touchType == 'direct') {
+      canvas.removeEventListener('touchmove', stopScroll);
+      return;
+    } else if(e.touches[0].touchType == 'stylus') {
+      canvas.addEventListener('touchmove', stopScroll, { passive: false });
+    }
 
     let X = ~~(e.changedTouches[0].clientX - rectX);
     let Y = ~~(e.changedTouches[0].clientY - rectY);
@@ -136,26 +163,40 @@
     draw(pointerX, pointerY, X, Y, thickness * thicknessCoefficient);
   });
 
-  // タッチされてもスクロールしないように
-  canvas.addEventListener('touchmove', function(e) {
-    e.preventDefault();
-  }, { passive: false });
-
   let ctx = canvas.getContext('2d');
   ctx.beginPath();
   ctx.fillStyle = "#f5f5f5";
   ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
+  socket.on('send user', function (msg) {
+    drawCore(msg.x1, msg.y1, msg.x2, msg.y2, msg.color, msg.thickness);
+  });
+
+  socket.on('clear user', () => {
+    allClear();
+  });
+
   function draw(x1, y1, x2, y2, thickness) {
+    socket.emit('server send', { x1: x1, y1: y1, x2: x2, y2: y2, color: defaultColor, thickness: thickness});
+    drawCore(x1, y1, x2, y2, defaultColor, thickness);
+  };
+
+  function drawCore(x1, y1, x2, y2, color, thickness) {
     ctx.beginPath();
     ctx.globalAlpha = defaultAlpha;
 
     ctx.moveTo(x1, y1);
     ctx.lineTo(x2, y2);
     ctx.lineCap = "round";
-    ctx.lineWidth = thickness * 4;
-    
-    ctx.strokeStyle = defaultColor;
+    ctx.lineWidth = thickness;
+    ctx.strokeStyle = color;
+
     ctx.stroke();
   };
+
+  function allClear() {
+    ctx.beginPath();
+    ctx.fillStyle = "#f5f5f5";
+    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+  }
 })();
