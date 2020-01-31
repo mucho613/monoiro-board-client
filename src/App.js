7e92ef13811c3b723284b7a163bbf487373b6767
Daisuke Yamamura <mucho613@gmail.com>
2020-02-01 06:40:43 +0900

    UIを分離

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React from 'react';
 import './App.css';
 
 import io from 'socket.io-client';
-import Pickr from '@simonwep/pickr/dist/pickr.es5.min';
-import '@simonwep/pickr/dist/themes/nano.min.css';
+import Toolbox from './ToolBox';
 
 class App extends React.Component {
   canvasWidth = 2000;
@@ -20,80 +19,21 @@ class App extends React.Component {
 
       selectedTool: 1,
       penColor: "#555555",
-      drawColor: "#555555",
+      eraserColor: "#f5f5f5",
       defaultAlpha: 1.0,
 
       penThicknessCoefficient: 16,
-      eraserThicknessCoefficient: 64,
-      thicknessCoefficient: 16
+      eraserThicknessCoefficient: 64
     }
-
-    this.componentDidMount = this.componentDidMount.bind(this);
   }
 
   componentDidMount() {
-    //HTML上の canvas タグを取得
     this.canvas = document.getElementById('canvas');
     this.canvasContext = this.canvas.getContext('2d');
+
+    this.downloadLink = document.getElementById('download-link');
     
-    // 強制リロードさせてキャッシュクリア
-    window.onpageshow = e => e.persisted ? window.location.reload() : null;
-
-    const pickr = Pickr.create({
-      el: '.color-picker',
-      theme: 'nano', // or 'monolith', or 'nano'
-
-      swatches: [
-        'rgba(244, 67, 54, 1)',
-        'rgba(233, 30, 99, 0.95)',
-        'rgba(156, 39, 176, 0.9)',
-        'rgba(103, 58, 183, 0.85)',
-        'rgba(63, 81, 181, 0.8)',
-        'rgba(33, 150, 243, 0.75)',
-        'rgba(3, 169, 244, 0.7)',
-        'rgba(0, 188, 212, 0.7)',
-        'rgba(0, 150, 136, 0.75)',
-        'rgba(76, 175, 80, 0.8)',
-        'rgba(139, 195, 74, 0.85)',
-        'rgba(205, 220, 57, 0.9)',
-        'rgba(255, 235, 59, 0.95)',
-        'rgba(255, 193, 7, 1)'
-      ],
-
-      components: {
-        // Main components
-        preview: true,
-        opacity: true,
-        hue: true,
-
-        // Input / output Options
-        interaction: {
-          hex: true,
-          rgba: true,
-          hsla: true,
-          hsva: false,
-          cmyk: false,
-          input: true,
-          clear: false,
-          save: true
-        }
-      }
-    });
-
-    let initImage = '';
-
-    pickr.on('init', instance => {
-    }).on('change', color => {
-      this.setState({
-        penColor: color.toHEXA().toString(),
-        drawColor: color.toHEXA().toString()
-      });
-    }).on('swatchselect', color => {
-      this.setState({
-        penColor: color.toHEXA().toString(),
-        drawColor: color.toHEXA().toString()
-      });
-    });
+    window.onpageshow = e => e.persisted && window.location.reload();
 
     let stopScroll = e => e.preventDefault();
 
@@ -134,8 +74,16 @@ class App extends React.Component {
       let X = ~~(e.clientX - rectX);
       let Y = ~~(e.clientY - rectY);
 
+      let thicknessCoefficient = this.state.selectedTool === 1
+        ? this.state.penThicknessCoefficient
+        : this.state.eraserThicknessCoefficient;
+
+      let drawColor = this.state.selectedTool === 1
+        ? this.state.penColor
+        : this.state.eraserColor;
+
       if(penGrounded) {
-        this.draw(pointerX, pointerY, X, Y, 0.1 * this.state.thicknessCoefficient, this.state.drawColor);
+        this.draw(pointerX, pointerY, X, Y, 0.1 * thicknessCoefficient, drawColor);
       }
 
       pointerX = X;
@@ -148,7 +96,15 @@ class App extends React.Component {
       let X = ~~(e.clientX - rectX);
       let Y = ~~(e.clientY - rectY);
 
-      this.draw(pointerX, pointerY, X, Y, 0.1 * this.state.thicknessCoefficient, this.state.drawColor);
+      let thicknessCoefficient = this.state.selectedTool === 1
+        ? this.state.penThicknessCoefficient
+        : this.state.eraserThicknessCoefficient;
+
+      let drawColor = this.state.selectedTool === 1
+        ? this.state.penColor
+        : this.state.eraserColor;
+
+      this.draw(pointerX, pointerY, X, Y, 0.1 * thicknessCoefficient, drawColor);
     });
 
     this.canvas.addEventListener('touchstart', e => {
@@ -192,8 +148,16 @@ class App extends React.Component {
           prevForce = currentForce;
         }
       }
+
+      let thicknessCoefficient = this.state.selectedTool === 1
+        ? this.state.penThicknessCoefficient
+        : this.state.eraserThicknessCoefficient;
+
+      let drawColor = this.state.selectedTool === 1
+        ? this.state.penColor
+        : this.state.eraserColor;
       
-      this.draw(pointerX, pointerY, X, Y, thickness * this.state.thicknessCoefficient, this.state.drawColor);
+      this.draw(pointerX, pointerY, X, Y, thickness * thicknessCoefficient, drawColor);
 
       pointerX = X;
       pointerY = Y;
@@ -224,15 +188,22 @@ class App extends React.Component {
         prevForce = currentForce;
       }
 
-      this.draw(pointerX, pointerY, X, Y, thickness * this.state.thicknessCoefficient, this.state.drawColor);
+      let thicknessCoefficient = this.state.selectedTool === 1
+        ? this.state.penThicknessCoefficient
+        : this.state.eraserThicknessCoefficient;
+
+      let drawColor = this.state.selectedTool === 1
+        ? this.state.penColor
+        : this.state.eraserColor;
+
+      this.draw(pointerX, pointerY, X, Y, thickness * thicknessCoefficient, drawColor);
     });
 
     this.socket.on('init', base64 => {
-      initImage = base64.imageData;
-
+      let initImage = base64.imageData;
       const img = new Image();
       img.src = initImage;
-      setTimeout(() => { this.canvasContext.drawImage(img, 0, 0) }, 500);
+      setTimeout(() => this.canvasContext.drawImage(img, 0, 0), 500);
     });
 
     this.socket.on('send user', msg => {
@@ -241,13 +212,17 @@ class App extends React.Component {
         this.drawCore(msg.x1, msg.y1, msg.x2, msg.y2, msg.color, msg.thickness);
       }
     });
-
-    this.socket.on('clear user', this.allClear);
   }
 
-  allClearEmit = () => {
-    this.allClear();
-    this.socket.emit('clear send');
+  handleToolChange = tool => this.setState({selectedTool: tool});
+  handlePenColorChange = color => this.setState({penColor: color});
+  handlePenThicknessChange = thickness => this.setState({penThicknessCoefficient: thickness});
+  handleEraserThicknessChange = thickness => this.setState({eraserThicknessCoefficient: thickness});
+  handleLeftyChange = isLefty => this.setState({leftyUi: isLefty});
+  handleDownload = () => {
+    this.downloadLink.href = this.canvas.toDataURL('image/png');
+    this.downloadLink.download = "monoiro.png";
+    this.downloadLink.click();
   }
 
   draw = (x1, y1, x2, y2, thickness, drawColor) => {
@@ -276,45 +251,6 @@ class App extends React.Component {
     this.canvasContext.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
   }
 
-  download = () => {
-    const downloadLink = document.getElementById('download-link');
-
-    downloadLink.href = this.canvas.toDataURL('image/png');
-    downloadLink.download = "monoiro.png";
-    downloadLink.click();
-  }
-
-  penSelected = (e) => {
-    this.setState(state => ({
-      drawColor: state.penColor,
-      selectedTool: 1,
-      thicknessCoefficient: state.penThicknessCoefficient
-    }));
-  }
-
-  eraserSelected = (e) => {
-    console.log(this.canvasContext);
-    this.setState(state => ({
-      drawColor: "#f5f5f5",
-      selectedTool: 2,
-      thicknessCoefficient: state.eraserThicknessCoefficient
-    }));
-  }
-
-  penThicknessChanged = (e) => {
-    this.setState({
-      penThicknessCoefficient: Number(e.target.value),
-      thicknessCoefficient: Number(e.target.value)
-    });
-  }
-
-  eraserThicknessChanged = (e) => {
-    this.setState({
-      eraserThicknessCoefficient: Number(e.target.value),
-      thicknessCoefficient: Number(e.target.value)
-    });
-  }
-
   render() {
     return (
       <div className="App">
@@ -324,28 +260,15 @@ class App extends React.Component {
           <button onClick={e => this.setState({splashWindowIsVisible: false})}>閉じる</button>
         </div>
         
-        <div id="ui" className={this.state.leftyUi ? 'overlay lefty' : 'overlay'}>
-          <a id="download-link"></a>
-          <button onClick={this.penSelected} className={(this.state.selectedTool === 1 ? 'pen-btn active' : 'pen-btn')}>ペン</button>
-          <button onClick={this.eraserSelected} className={(this.state.selectedTool === 2 ? 'eraser-btn active' : 'eraser-btn')}>消しゴム</button>
-          <button onClick={this.allClearEmit} className="all-erase-btn" disabled>全消し</button>
-          <button onClick={this.download}>ダウンロード</button>
-          <div className="color-picker"></div>
-        
-          <div>
-            <div>ペンの太さ: {this.state.penThicknessCoefficient}</div>
-            <input onChange={this.penThicknessChanged} defaultValue={this.state.penThicknessCoefficient} type="range" min="16" max="256"></input>
-          </div>
-          
-          <div>
-            <div>消しゴムの太さ: {this.state.eraserThicknessCoefficient}</div>
-            <input onChange={this.eraserThicknessChanged} defaultValue={this.state.eraserThicknessCoefficient} type="range" min="32" max="512"></input>
-          </div>
-          
-          <div>
-            <input onChange={e => this.setState({leftyUi: e.target.checked})} type="checkbox"></input>左利き
-          </div>
-        </div>
+        <Toolbox
+          toolState={this.state}
+          onToolChange={this.handleToolChange}
+          onPenColorChange={this.handlePenColorChange}
+          onPenThicknessChange={this.handlePenThicknessChange}
+          onEraserThicknessChange={this.handleEraserThicknessChange}
+          onLeftyChange={this.handleLeftyChange}
+          onDownload={this.handleDownload}
+        />
         
         <div onScroll={e => this.setState({scrolled: true})} className="canvas-wrapper">
           <canvas id="canvas" width="2000" height="2000"></canvas>
